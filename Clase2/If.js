fca6a2f7255d9b9baca7fd6832c6ebc410a09546
Agustin Sarquis <arsarquis@gmail.com>
2021-09-07 16:49:02 -0300

    clase2

---
+++ Clase2/If.js
@@ -0,0 +1,9 @@
+var nombreIngresado =prompt("Ingresar Nombre")
+var apellidoIngresado =prompt("Ingresar Apellido")
+
+if((nombreIngresado !="") && (apellidoIngresado !="")){
+    alert("Nombre y Apellido: "+nombreIngresado +" "+ apellidoIngresado)
+}else{
+    alert("Error: Ingresar Nombre y Apellido")
+}
+
