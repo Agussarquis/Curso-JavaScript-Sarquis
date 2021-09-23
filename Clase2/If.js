3a06901b14d38008a3f82ef90d46513fa5ad0e2f
Agussarquis <86983883+Agussarquis@users.noreply.github.com>
2021-09-23 14:59:12 -0300

    Delete Clase2 directory

--- Clase2/If.js
+++
@@ -1,9 +0,0 @@
-var nombreIngresado =prompt("Ingresar Nombre")
-var apellidoIngresado =prompt("Ingresar Apellido")
-
-if((nombreIngresado !="") && (apellidoIngresado !="")){
-    alert("Nombre y Apellido: "+nombreIngresado +" "+ apellidoIngresado)
-}else{
-    alert("Error: Ingresar Nombre y Apellido")
-}
-
